7c915e6a8b84418927d2494e9288ebf8767c8785
AhmedYehiaa <ahmed.yehia@cognitev.com>
2019-09-03 19:42:47 +0200

    Update project structure

---
+++ service/helpers.js
@@ -0,0 +1,43 @@
+var faker = require('faker');
+
+const generateFakeAdvisors = () => {
+  const advisors = [];
+  for (let i = 0; i < 500; i++) {
+    const advisor = {
+      id: faker.random.uuid(),
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      avatar: faker.image.avatar(),
+      language: faker.random.arrayElement(["English", "Arabic", "German", "Spanish", "Chinese"]),
+      status: faker.random.arrayElement(["Online", "Offline"]),
+      numOfReviews: faker.random.number({ min: 0, max: 99 })
+    };
+    advisors.push(advisor);
+  }
+  return advisors;
+};
+
+const sort = (list = [], sortBy, type) => {
+  let sortedList = [];
+  if (type === "asc")
+    sortedList = list.sort((a, b) => (a[sortBy] > b[sortBy]) ? 1 : -1);
+  else
+    sortedList = list.sort((a, b) => (a[sortBy] < b[sortBy]) ? 1 : -1);
+  return sortedList;
+};
+
+
+const filter = (list = [], filterBy) => {
+  const { status, language } = filterBy;
+  let filteredList = [];
+  filteredList = list.filter(item => (
+    (language === "any" || item.language.toLowerCase() === language.toLowerCase())
+    && (status === "any" || item.status.toLowerCase() === status.toLowerCase())));
+  return filteredList;
+};
+
+module.exports = {
+  generateFakeAdvisors,
+  sort,
+  filter
+}
--- service/index.js
+++ service/index.js
@@ -3,12 +3,11 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 const bodyParser = require("body-parser");
 const path = require("path");
-var faker = require('faker');
 
+const { generateFakeAdvisors, sort, filter } = require("./helpers");
 const PORT = process.env.PORT || 8000;
 
 const app = express();
-
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,18 +23,9 @@ app.get("/", (req, res) => {
 });
 
 app.get("/advisors", (req, res) => {
-  const advisors = [];
-  for (let i = 0; i < 500; i++) {
-    const advisor = {
-      id: faker.random.uuid(),
-      name: faker.name.findName(),
-      email: faker.internet.email(),
-      avatar: faker.image.avatar(),
-      language: faker.random.arrayElement(["English", "Arabic", "German", "Spanish", "Chinese"]),
-      numOfReviews: faker.random.number({ min: 0, max: 99 })
-    };
-    advisors.push(advisor);
-  }
+  let advisors = generateFakeAdvisors();
+  advisors = sort(advisors, "numOfReviews", "desc");
+  advisors = filter(advisors, { language: "english", status: "offline" });
   res.json(advisors);
 });
 
