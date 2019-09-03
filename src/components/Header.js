224d36389fffdd992df91c9aec28849512eae11b
AhmedYehiaa <ahmed.yehia@cognitev.com>
2019-09-04 01:48:13 +0200

    Add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+
+const HeaderWrapper = styled.header`
+  position: fixed;
+  z-index: 1000;
+  width: 100%;
+  background: linear-gradient(135deg, #7363dc, #bd63dc);
+  padding: 0 20px;
+
+`;
+
+const Header = () => {
+  return (
+    <HeaderWrapper>
+      Header
+    </HeaderWrapper>
+  );
+}
+
+export default Header;
