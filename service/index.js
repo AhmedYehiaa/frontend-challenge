3edaf4eef00eb90c557c3cf5cb75bf45d0e86ad9
AhmedYehiaa <ahmed.yehia@cognitev.com>
2019-09-03 23:10:22 +0200

    Add allowing CORS origin

--- service/index.js
+++ service/index.js
@@ -13,6 +13,14 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Allow CORS origin
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.static(path.join(__dirname, "build")));
 
 // Swagger docs
